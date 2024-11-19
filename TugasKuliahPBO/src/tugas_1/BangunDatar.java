8e3cc3573ef5e212f5a2e807d9899af395541ba2
radinals <radinalsaragih@gmail.com>
2024-11-19 22:39:04 +0700

    cleanup comments

--- TugasKuliahPBO/src/tugas_1/BangunDatar.java
+++ TugasKuliahPBO/src/tugas_1/BangunDatar.java
@@ -1,15 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package tugas_1;
 
 import java.util.Scanner;
 
-/**
- *
- * @author rss
- */
 public class BangunDatar {
         
       final static float PI = 3.14f;
--- TugasKuliahPBO/src/tugas_5/BangunDatar.java
+++ TugasKuliahPBO/src/tugas_5/BangunDatar.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package tugas_5;
 
-/**
- *
- * @author rss
- */
 public class BangunDatar {
 		private float luas() {
 			return 0;
